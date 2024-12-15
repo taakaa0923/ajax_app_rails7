b5c2667e359ab3f9b64b69b7db39f5c68f0d5a0d
taakaa0923 <napoyama3sei@gmail.com>
2024-12-15 10:02:03 +0900

    コントローラーにリクエストを送信する

---
+++ app/javascript/memo.js
@@ -0,0 +1,14 @@
+function post (){
+  const form = document.getElementById("form");
+  form.addEventListener("submit",(e) => {
+    e.preventDefault();
+    const formDate = new FormData(form);
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/posts", true);
+    XHR.responseType = "json" ;
+    XHR.send(formDate);
+  });
+};
+
+window.addEventListener(`turbo:load` , post);
+
